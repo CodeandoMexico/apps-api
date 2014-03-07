54f6baf5fb5f513ceb81e4486d8ab0f4b9fa75cb
Adrian Rangel <adrian.rangel@gmail.com>
2014-03-07 10:34:08 -0600

    Create rails-api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+AppsApi::Application.config.secret_token = 'cedca7ccf085c6ff075200019fb8a0ec6e47cbbdf17759dc516165bd39e126f3a1485a8688f65481946b8943c129e15550a59bcf008d0dd5af718d87e8392669'
