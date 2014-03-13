1fa0eadf2e49a2e06703b47f782d089d947d6cb2
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2014-03-12 18:31:07 -0600

    Add technologies field to app serializer

--- app/serializers/app_serializer.rb
+++ app/serializers/app_serializer.rb
@@ -1,3 +1,3 @@
 class AppSerializer < ActiveModel::Serializer
-  attributes :id, :dataset_uris, :challenge_url, :codebase_url, :demo_url, :description, :name, :creators, :organization, :location, :logo_url, :visible
+  attributes :id, :dataset_uris, :challenge_url, :codebase_url, :demo_url, :description, :name, :creators, :organization, :location, :logo_url, :visible, :technologies
 end
