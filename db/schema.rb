a47b75538a93d94dbbada9ff7b41a9f229be4a6d
Miguel Martinez <miguelmtz93@gmail.com>
2014-03-12 18:10:48 -0600

    Add techonologies field to app model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140308050955) do
+ActiveRecord::Schema.define(version: 20140312230250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140308050955) do
     t.boolean  "visible"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "technologies"
   end
 
 end
